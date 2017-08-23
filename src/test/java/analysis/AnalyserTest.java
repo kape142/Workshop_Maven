2731190c97747011eaf601a88e6708efdad7cccc
[kape142] <[kape142@hotmail.com]>
2017-08-23 10:58:00 +0200

    maven

--- src/test/java/analysis/AnalyserTest.java
+++ src/test/java/analysis/AnalyserTest.java
@@ -8,34 +8,33 @@ import static org.junit.Assert.assertEquals;
  * @author nilstes
  */
 public class AnalyserTest {
-    
+
     Analyser analyser = new Analyser();
-    
+
     @Test
     public void testGoodSentiment() {
         assertEquals(3, analyser.findSentiment("I am so happy"));
         assertEquals(3, analyser.findSentiment("What a beautiful day"));
     }
-    
+
     @Test
     public void testVeryGoodSentiment() {
         assertEquals(4, analyser.findSentiment("Happy good beautiful"));
     }
-    
+
     @Test
     public void testBadSentiment() {
         assertEquals(1, analyser.findSentiment("I hate you"));
         assertEquals(1, analyser.findSentiment("Work was really bad today"));
     }
-    
+
     @Test
     public void testVeryBadSentiment() {
-        assertEquals(0, analyser.findSentiment("You are very smart"));
-//        assertEquals(0, analyser.findSentiment("Bad"));
+        assertEquals(0, analyser.findSentiment("Bad"));
     }
-    
+
     @Test
-    public void testSoSoSentiment() {    
+    public void testSoSoSentiment() {
         assertEquals(2, analyser.findSentiment("Well well"));
         assertEquals(2, analyser.findSentiment("Yet another day"));
     }
